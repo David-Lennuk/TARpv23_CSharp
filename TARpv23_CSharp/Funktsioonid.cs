b6ea34a13c40650c8c0eaeaf4a99f9f52829e1c6
opilane <opilane@HP-CZC2349HT2>
2024-08-26 16:19:26 +0300

    Add project files.

---
+++ TARpv23_CSharp/Funktsioonid.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARpv23_CSharp
+{
+    internal class Funktsioonid
+    {
+        public static void Tere(string nimi)
+        {
+            Console.WriteLine("Tere kallis {0}", nimi);
+        }
+
+        public static int Liitmine(int arv1, int arv2)
+        { 
+            return arv1 + arv2; 
+        }
+        //Loo Arvuta() funktsioon, mis sõltub 3 parameetrist: tehe, arv1, arv2. Kasuta if konstruktsion. Tulemus kuva ekraanile.
+
+        public static double Arvuta(string operatsion, int arv1, int arv2)
+        {
+            int Arve = 0;
+            if (operatsion == "+")
+            {
+                Arve = arv1 + arv2;
+            }
+            else if (operatsion == "-")
+            {
+                Arve = arv1 - arv2;
+            }
+            else if (operatsion == "/")
+            {
+                Arve = arv1 / arv2;
+            }
+            else if (operatsion == "*")
+            {
+                Arve = arv1 * arv2;
+            }
+            return Arve;
+        }
+    }
+
+}
---
+++ TARpv23_CSharp/MainClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARpv23_CSharp
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Hello, world!");
+            string nimetus = "Python";
+            Console.WriteLine("Hello, {0}", nimetus);
+            Funktsioonid.Tere(nimetus);
+            Console.Write("Sisesta esimene arv: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Sisesta teine arv: ");
+            int b = int.Parse(Console.ReadLine());
+            int vastus = Funktsioonid.Liitmine(b, a);
+            Console.WriteLine(vastus);
+            double arv = 5.123456;
+            vastus = Funktsioonid.Liitmine(a, (int)arv);
+            Console.WriteLine(vastus);
+            char taht = 'A';
+            if (vastus == 0)
+            {
+                Console.WriteLine(taht);
+            }
+            else
+            {
+                Console.WriteLine(vastus);
+            }
+            Console.WriteLine(Funktsioonid.Arvuta("*", 5, 5));
+        }
+
+    }
+}
