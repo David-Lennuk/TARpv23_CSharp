e86a9db8ffe4a6f6c8c2e22a91f07796514229a2
David-Lennuk <153904926+David-Lennuk@users.noreply.github.com>
2024-08-28 14:19:45 +0300

    Update MainClass.cs

--- TARpv23_CSharp/MainClass.cs
+++ TARpv23_CSharp/MainClass.cs
@@ -5,107 +5,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace TARpv23_CSharp
 {
     public class MainClass
     {
         public static void Main(string[] args)
         {
+            Random random = new Random();
             Console.OutputEncoding = Encoding.UTF8;
-
-            // Задача 1 Проверка имени и возраста Juku для покупки билета
-            Console.Write("Sisesta oma nimi: ");
-            string nimi = Console.ReadLine();
-            if (nimi.ToLower() == "juku")
-            {
-                Console.Write("Sisesta vanus: ");
-                int vanus = int.Parse(Console.ReadLine());
-                if (vanus < 0 || vanus > 100)
-                {
-                    Console.WriteLine("Viga andmetega!");
-                }
-                else if (vanus < 6)
-                {
-                    Console.WriteLine("Tasuta pilet");
-                }
-                else if (vanus >= 6 && vanus <= 14)
-                {
-                    Console.WriteLine("Lastepilet");
-                }
-                else if (vanus >= 15 && vanus <= 65)
-                {
-                    Console.WriteLine("Täispilet");
-                }
-                else if (vanus > 65)
-                {
-                    Console.WriteLine("Sooduspilet");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Tere, {0}! See programm on mõeldud ainult Juku jaoks.", nimi);
-            }
-
-            // Задача 2 Сообщение о том, что два человека сегодня соседи по парте
-            Console.Write("Sisesta esimese inimese nimi: ");
-            string nimi1 = Console.ReadLine();
-            Console.Write("Sisesta teise inimese nimi: ");
-            string nimi2 = Console.ReadLine();
-            Console.WriteLine("{0} ja {1} on täna pinginaabrid!", nimi1, nimi2);
-
-            // Задача 3 Расчет площади комнаты и стоимости ремонта
-            Console.Write("Sisesta esimese seina pikkus meetrites: ");
-            double sein1 = double.Parse(Console.ReadLine());
-            Console.Write("Sisesta teise seina pikkus meetrites: ");
-            double sein2 = double.Parse(Console.ReadLine());
-            double pindala = sein1 * sein2;
-            Console.WriteLine("Ristkülikukujulise toa põranda pindala on {0} ruutmeetrit.", pindala);
-
-            Console.Write("Kas soovid teha remonti (jah/ei): ");
-            string remontVastus = Console.ReadLine().ToLower();
-            if (remontVastus == "jah")
-            {
-                Console.Write("Sisesta ruutmeetri hind: ");
-                double hind = double.Parse(Console.ReadLine());
-                double koguMaksumus = pindala * hind;
-                Console.WriteLine("Põranda vahetamise hind on {0} eurot.", koguMaksumus);
-            }
-
-            // Задача 4 Нахождение начальной цены с учетом 30% скидки
-            Console.Write("Sisesta hind pärast 30% allahindlust: ");
-            double hindPärastAllahindlust = double.Parse(Console.ReadLine());
-            double algneHind = hindPärastAllahindlust / 0.7;
-            Console.WriteLine("Alghind enne allahindlust oli {0} eurot.", algneHind);
-
-            // Задача 5 Проверка температуры на соответствие комфортному уровню
-            Console.Write("Sisesta toatemperatuur kraadides: ");
-            double temperatuur = double.Parse(Console.ReadLine());
-            if (temperatuur > 18)
-            {
-                Console.WriteLine("Temperatuur on sobiv.");
-            }
-            else
-            {
-                Console.WriteLine("Temperatuur on liiga madal.");
-            }
-
-            // Дополнительно, ранее добавленные функции
+            Console.WriteLine("Hello, world!");
             string nimetus = "Python";
             Console.WriteLine("Hello, {0}", nimetus);
             Funktsioonid.Tere(nimetus);
-
             Console.Write("Sisesta esimene arv: ");
             int a = int.Parse(Console.ReadLine());
             Console.Write("Sisesta teine arv: ");
             int b = int.Parse(Console.ReadLine());
             int vastus = Funktsioonid.Liitmine(b, a);
             Console.WriteLine(vastus);
-
             double arv = 5.123456;
             vastus = Funktsioonid.Liitmine(a, (int)arv);
             Console.WriteLine(vastus);
-
             char taht = 'A';
             if (vastus == 0)
             {
@@ -115,8 +35,141 @@ namespace TARpv23_CSharp
             {
                 Console.WriteLine(vastus);
             }
-
             Console.WriteLine(Funktsioonid.Arvuta("*", 5, 5));
+
+
+            //I.osa Andmetüübid, Alamfunktsioonid, IF
+            string[] nimed = new string[5] { "Anna", "Inna", "Oksana", "Pavel", "Karl" };
+            //1.V
+            for (int i = 0; i < nimed.Length; i++)
+            {
+                Console.WriteLine(nimed[i]);
+            }
+            //2.v
+            foreach (string nimi in nimed)
+            {
+                Console.WriteLine(nimi);
+            }
+            //3.v
+            int n = 0;
+            while (n < nimed.Length)
+            {
+                Console.WriteLine(nimed[n]);
+                n++;
+            }
+            //4.v
+            do
+            {
+                Console.WriteLine(nimed[n - 1]);
+                n--;
+            } while (n! > 0); //!
+
+
+
+
+
+
+
+            for (int i = 0; i < 7; i++)
+            {
+                int paev_nr = random.Next(-4, 30);
+                string paeva_nimetus = Funktsioonid.Paevad(paev_nr);
+                Console.WriteLine(paeva_nimetus);
+            }
+
+
+
+            try
+            {
+
+
+
+
+                Console.WriteLine("Mis on sinu pikkus?");
+                double pikkus = Double.Parse(Console.ReadLine());
+                string vastus1 = Funktsioonid.Pikkuse_analuus(pikkus);
+                Console.WriteLine("Sinu pikkus on {0}, sa oled {1}", pikkus, vastus1);
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.ToString());
+            }
+
+
+
+            ////1* Loo  juhuslikult arvud N ja M ja sisesta massiivi arvud N'st M'ni. Trüki arvude ruudud ekraanile. N ja M arvud on vahemikus (-100,100).
+            int N = random.Next(-100, 101);
+            int M = random.Next(-100, 101);
+            int[] arvud;
+
+            if (N < M)
+            {
+                arvud = Funktsioonid.Arvude_massive(N, M);
+            }
+            else
+            {
+                arvud = Funktsioonid.Arvude_massive(N, M);
+            }
+            foreach (int item in arvud)
+            {
+                Console.WriteLine(item * item);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //int N = random.Next(-100, 101);
+            //int M = random.Next(-100, 101);
+
+            //// Определение начального и конечного значения для заполнения массива
+            //int start = Math.Min(N, M);
+            //int end = Math.Max(N, M);
+
+            //// Вывод случайных чисел N и M
+            //Console.WriteLine($"Случайные числа: N = {N}, M = {M}");
+
+            //// Вычисление количества элементов в массиве
+            //int size = end - start + 1;
+            //int[] array = new int[size];
+
+            //// Заполнение массива числами от start до end
+            //for (int i = 0; i < size; i++)
+            //{
+            //    array[i] = start + i;
+            //}
+
+            //// Вывод квадратов всех чисел массива
+            //Console.WriteLine("Квадраты чисел от N до M:");
+            //foreach (int num in array)
+            //{
+            //    Console.WriteLine($"{num}^2 = {num * num}");
+            //}
+
         }
+
     }
 }
